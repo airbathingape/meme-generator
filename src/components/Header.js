18f2c595e44777eba81e9dced6cc53fe379ca96e
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-29 10:51:57 +0900

    added header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
+import './Header.css'
 import troll from "../images/troll-face.png";
 
 function Header() {
   return (
-    <div>
-      <div className="nav--header">
+    <div className="nav--header">
+      <div className='nav--meme'>
         <img src={troll} alt="face" />
         <h1>Meme Generator</h1>
       </div>
