61beab3d4c9b943b3939af8bda06ad1b2424fbc6
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-30 11:39:11 +0900

    added random image function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,22 @@
 import './Main.css'
+import data from './memesData'
 
 function Main() {
+
+  function getImage() {
+
+    const memesArray = data.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    console.log(url)
+}
+
   return (
     <main className='main'>
       <form className='main--input'>
         <input type="text" className='form--input' />
         <input type="text" className='form--input' />
-        <button className='button--input'>Get a new meme image</button>
+        <button onClick={getImage} className='button--input'>Get a new meme image</button>
       </form>
     </main>
   )
