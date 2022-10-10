133c9a9af7a8cd270a9bd5a1b67e33c93355d1dd
Daniel Domingo <domingo.daniel21@gmail.com>
2022-10-10 15:36:41 +0900

    added function

--- src/components/Main.js
+++ src/components/Main.js
@@ -28,14 +28,27 @@ function Main() {
 
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target
+    setMeme(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   return (
     <main className='main'>
       <div className='main--input'>
-        <input type="text" className='form--input' placeholder='Input Text'/>
-        <input type="text" className='form--input' placeholder='Input Text'/>
+        <input type="text" className='form--input' placeholder='Input Text' name='topText' value={meme.topText} onChange={handleChange} />
+        <input type="text" className='form--input' placeholder='Input Text' name='bottomText' value={meme.bottomText} onChange={handleChange} />
         <button onClick={getImage} className='button--input'>Get a new meme image</button>
       </div>
-      <img src={meme.randomImage} alt="Meme" className='meme--image' />
+
+      <div className="meme">
+        <img src={meme.randomImage} alt="Meme" className='meme--image' />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   )
 }
